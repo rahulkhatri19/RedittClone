53f2f3ef0d2a5aa897fc11f8168f0322d843c5d4
Rahul Khatri <khatri.rahul019@gmail.com>
2023-05-01 16:42:36 +0530

    Lib issues fix

---
+++ app/src/main/java/in/explorer/redittclone/HomePage.kt
@@ -0,0 +1,16 @@
+package `in`.explorer.redittclone
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
+
+
+class HomePage: ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Text("Hello world!")
+        }
+    }
+}
